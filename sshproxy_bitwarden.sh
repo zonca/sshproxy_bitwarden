1e9441a8b1b619bdb7c2fcaa537b6f4d8f6c30d7
Andrea Zonca <code@andreazonca.com>
2026-01-13 16:43:41 -0800

    Add modification copyright notice

--- sshproxy_bitwarden.sh
+++ sshproxy_bitwarden.sh
@@ -4,6 +4,7 @@
 # SSH Proxy (sshproxy), Copyright (c) 2019, The Regents of the University of California,
 # through Lawrence Berkeley National Laboratory (subject to receipt of any required
 # approvals from the U.S. Dept. of Energy).  All rights reserved.
+# Modifications Copyright (c) 2026, Andrea Zonca. All rights reserved.
 # 
 # If you have questions about your rights to use or distribute this software,
 # please contact Berkeley Lab's Intellectual Property Office at  IPO@lbl.gov.
